384b7f14167363d832310176b8117f23c4f458fd
Jeff Simpson <jeff1993@uga.edu>
2013-07-11 10:38:18 -0400

    updated base gui, moved java script to bottom of page for faster rendering

--- projects/Symfony/src/Git/Bundle/GuiBundle/Resources/views/Default/base.html.php
+++ projects/Symfony/src/Git/Bundle/GuiBundle/Resources/views/Default/base.html.php
@@ -11,8 +11,49 @@
       <title>Git Gui</title>
       <!-- Bootstrap -->
       <link href="http://twitter.github.com/bootstrap/assets/css/bootstrap.css" rel="stylesheet">
+      <link href="http://twitter.github.com/bootstrap/assets/css/bootstrap-responsive.css" rel="stylesheet">
    </head>
-   <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
+   <body>
+   
+    <div class="navbar navbar-fixed-top">
+      <div class="navbar-inner">
+        <div class="container">
+          <button type="button" class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="brand" href="./index.html">Git Gui</a>
+          <div class="nav-collapse collapse">
+            <ul class="nav">
+              <li class="active">
+               <a href="index">Home</a>
+              </li>
+              <li class="">
+               <a href="create">Create Users</a>
+              </li>
+              <li class="">
+                <a href="group">Manage Groups</a>
+              </li>
+              <li class="">
+                <a href="repo">Manage Repositories</a>
+              </li>
+              <li class="">
+                <a href="../../../../../../../gitlist/index.php">View Repositories</a>
+              </li>
+              <li class="">
+                <a href="loggedOut">Log Out</a>
+              </li>
+           
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+   
+   
+   
+      <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
    <script src="js/bootstrap.js"></script>
    <script src="<?php echo $view['assets']->getUrl('js/jQuery.dualListBox-1.3.js') ?>" type="text/javascript" ></script>
    <script language="javascript" type="text/javascript">
@@ -23,41 +64,8 @@
       });
       
    </script>
-   </head>
-   <body>
-      <div class="navbar">
-         <div class="navbar-inner">
-            <ul class="nav">
-               <li><a href="index">Home</a></li>
-               <li class="dropdown">
-                  <a class="dropdown-toggle" data-toggle="dropdown" href="#">
-                  Users <b class="caret"></b>
-                  </a>
-                  <ul class="dropdown-menu">
-                     <li><a href="create">Create Users</a></li>
-                  </ul>
-               </li>
-               <li class="dropdown">
-                  <a class="dropdown-toggle" data-toggle="dropdown" href="#">
-                  Groups <b class="caret"></b>
-                  </a>
-                  <ul class="dropdown-menu">
-                     <li><a href="group">Manage Groups</a></li>
-                  </ul>
-               <li class="dropdown">
-                  <a class="dropdown-toggle" data-toggle="dropdown" href="#">
-                  Repos <b class="caret"></b>
-                  </a>
-                  <ul class="dropdown-menu">
-                     <li><a href="repo">Manage Repositories</a></li>
-                     <li><a href="../../../../../../../gitlist/index.php">View Repositories</a></li>
-                  </ul>
-               </li>
-               </li>
-               <li><a href="loggedOut">Log Out</a></li>
-            </ul>
-         </div>
-      </div>
+   
+
       <?php
          $view['slots']->output('title', 'Hello Application') ?>
    </body>
